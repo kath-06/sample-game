8a76ca59a61daa45d59748bc8d80e6d599f20603
kathbalbas@gmail.com <kathbalbas@gmail.com>
2023-07-28 17:28:31 +0800

    add tailwind directive for game card components

--- src/components/custom/GameCard.vue
+++ src/components/custom/GameCard.vue
@@ -1,6 +1,6 @@
 <template>
     <el-card
-        class="w-68 md:w-76 h-60 md:h-68 m-2 bg-sky-50 rounded-xl flex justify-center items-center border-sky-500 transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
+        class="primary-line-card"
         shadow="hover">
         <slot />
     </el-card>
