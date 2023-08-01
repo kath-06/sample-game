221ba1d6e3a1f9359e127f2c53aad3c1ca61ac14
kathbalbas@gmail.com <kathbalbas@gmail.com>
2023-08-01 14:37:13 +0800

    change app loading

--- src/layouts/LoadingLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <el-container>
-    <el-main class="main-body-light-primary">
-      <router-view />
-    </el-main>
-  </el-container>
-</template>
-
-<script lang="ts" setup name="LoadingLayout" />
