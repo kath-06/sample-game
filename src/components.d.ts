398fd4ba8ff6d6c4c706184f17f3212409a72fb2
kathbalbas@gmail.com <kathbalbas@gmail.com>
2023-08-23 11:01:31 +0800

    main table components - add pagination

--- src/components.d.ts
+++ src/components.d.ts
@@ -32,6 +32,7 @@ declare module '@vue/runtime-core' {
     ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElPagination: typeof import('element-plus/es')['ElPagination']
     ElRow: typeof import('element-plus/es')['ElRow']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     GameCard: typeof import('./components/custom/GameCard.vue')['default']
--- src/composables/element.ts
+++ src/composables/element.ts
@@ -13,3 +13,11 @@ export const messageBoxTypes = [
   { name: 'default', headerClass: 'msg-box-default-header',  msgClass: 'msg-box-default-text', msgType: '' },
   { name: 'warning', headerClass: 'msg-box-warning-header',  msgClass: 'msg-box-warning-text', msgType: 'warning' }
 ]
+
+export const tableTypes = [
+  { name: 'primary', tableClass: 'primary-table', headerClass: 'primary-table-header', headerRowClass: 'primary-table-header-row', rowClass: 'primary-table-row', dropdownClass: 'btn-primary-tbl-dropdown', dropdownItemClass: 'primary-tbl-dropdown-item', paginationClass: "primary-table-pagination" },
+  { name: 'default', tableClass: 'default-table', headerClass: 'default-table-header', headerRowClass: 'default-table-header-row', rowClass: 'default-table-row', dropdownClass: 'btn-default-tbl-dropdown', dropdownItemClass: 'default-tbl-dropdown-item', paginationClass: "default-table-pagination" },
+  { name: 'success', tableClass: 'success-table', headerClass: 'success-table-header', headerRowClass: 'success-table-header-row', rowClass: 'success-table-row', dropdownClass: 'btn-success-tbl-dropdown', dropdownItemClass: 'success-tbl-dropdown-item', paginationClass: "success-table-pagination" },
+  { name: 'danger', tableClass: 'danger-table', headerClass: 'danger-table-header', headerRowClass: 'danger-table-header-row', rowClass: 'danger-table-row', dropdownClass: 'btn-danger-tbl-dropdown', dropdownItemClass: 'danger-tbl-dropdown-item', paginationClass: "danger-table-pagination" },
+  { name: 'warning', tableClass: 'warning-table', headerClass: 'warning-table-header', headerRowClass: 'warning-table-header-row', rowClass: 'warning-table-row', dropdownClass: 'btn-warning-tbl-dropdown', dropdownItemClass: 'warning-tbl-dropdown-item', paginationClass: "warning-table-pagination" }
+]
